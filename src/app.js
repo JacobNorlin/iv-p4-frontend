e4fdc44a60b7353b52a1ff6886bbf1e80e5a429b
Nick Anderson <nja4@uw.edu>
2016-02-18 21:02:39 +0100

    Added tablesorter, maybe should remove to keep the frontend size small?

--- src/app.js
+++ src/app.js
@@ -1,3 +1,127 @@
 "use strict";
+var map;
+function initMap() {
+    $.ajax({
+        url: "http://localhost:3000/getTurbineLocations",
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function(jsonp) {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: jsonp[0]["lat"], lng: jsonp[0]["lng"]},
+                zoom: 8
+            });
+            $.each(jsonp, function(windmillTupleIndex) {
+                var windmillTuple = jsonp[windmillTupleIndex];
+                displayWindmill(windmillTuple["id"],
+                    windmillTuple["lat"], windmillTuple["lng"]);
+            });
+        },
+        error: function(err) {
+            console.log(err);
+            $.notify({
+                // options
+                message: 'GET Request failed!'
+            },{
+                // settings
+                type: 'danger'
+            }); }
+    });
+}
 
-console.log("hej");
+function displayWindmill(id, lat, lng) {
+    var myLatLng = {lat: lat, lng: lng};
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: "" + id
+    });
+    marker.addListener('click', function() {
+        map.setCenter(marker.getPosition());
+        displayTurbineInfo(id);
+    });
+}
+function displayTurbineInfo(id) {
+    $.ajax({
+        url: "http://localhost:3000/getTurbineDataFromdayById/" + id + "/2016/1/1",
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function(jsonp) {
+            $.notify({
+                // options
+                message: 'GET Request Success!'
+            },{
+                // settings
+                type: 'info'
+            });
+            $.each(jsonp, function(rowIndex) {
+                var rowTuple = jsonp[rowIndex];
+                var tr = $("<tr/>");
+                tr.append("<td>" + rowTuple["date"] + "</td>");
+                tr.append("<td>" + rowTuple["endTime"] + "</td>");
+                tr.append("<td>" + rowTuple["windSpeed"] + "</td>");
+                tr.append("<td>" + rowTuple["ac_primary_load"] + "</td>");
+                tr.append("<td>" + rowTuple["hugh_piggott"] + "</td>");
+                tr.append("<td>" + rowTuple["ac_primary_served"] + "</td>");
+                tr.append("<td>" + rowTuple["excess_electricity"] + "</td>");
+                tr.append("<td>" + rowTuple["unmet_load"] + "</td>");
+                tr.append("<td>" + rowTuple["capacity_shortage"] + "</td>");
+                tr.append("<td>" + rowTuple["inverter_input_power"] + "</td>");
+                tr.append("<td>" + rowTuple["inverter_output_power"] + "</td>");
+                tr.append("<td>" + rowTuple["recitifier_input_power"] + "</td>");
+                tr.append("<td>" + rowTuple["rectifier_output_power"] + "</td>");
+                tr.append("<td>" + rowTuple["battery_input_power"] + "</td>");
+                tr.append("<td>" + rowTuple["battery_state_of_charge"] + "</td>");
+                tr.append("<td>" + rowTuple["battery_energy_cost"] + "</td>");
+                $("#turbineTable").append(tr);
+            });
+            $("#turbineTable").tablesorter();
+        },
+        error: function(err) {
+            console.log(err);
+            $.notify({
+                // options
+                message: 'GET Request failed!'
+            },{
+                // settings
+                type: 'danger'
+            }); }
+        //beforeSend: setHeader
+    });
+    $("#modalHeader").text("Turbine: " + id);
+    $("#turbineInfoModal").modal("show");
+}
+
+
+
+/*
+$.ajax({
+    url: "http://localhost:3000/getTurbineDataFromdayById/1/2016/1/1",
+    type: 'GET',
+    crossDomain: true,
+    dataType: 'jsonp',
+    success: function(jsonp) {
+        $.notify({
+            // options
+            message: 'GET Request Success!'
+        },{
+            // settings
+            type: 'info'
+        });
+        console.log(jsonp);
+        $("#row").text(JSON.stringify(jsonp));
+    },
+    error: function(err) {
+        console.log(err);
+        $.notify({
+            // options
+            message: 'GET Request failed!'
+    },{
+        // settings
+        type: 'danger'
+    }); }
+    //beforeSend: setHeader
+});
+*/
+
