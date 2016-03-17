ff547376d8b661b05e396593443c1ac21908aaa4
JacobNA <jacob.norlinandersson@gmail.com>
2016-03-17 17:09:23 +0100

    merged in d3 map

--- src/app.js
+++ src/app.js
@@ -7,16 +7,22 @@ require('d3');
 require('jquery-ui');
 require('bootstrap-webpack');
 require('../css/index.css');
+import d3 from 'd3';
+import {queue} from 'd3-queue';
+
+import tzaJson from 'json!../resources/data/tza.json';
+import weatherJson from 'json!../resources/data/weather.json';
+
 
 import {HeatMap, parameters} from './HeatMap.js';
 import RequestHandler from './RequestHandler.js';
 
 
-var GoogleMapsLoader = require('google-maps'); // only for common js environments
-GoogleMapsLoader.KEY = 'AIzaSyAGe-_v3CJKidJo4RJEXAfVRrhVNnEebpU';
-GoogleMapsLoader.load(function(google) {
-    initMap();
-});
+// var GoogleMapsLoader = require('google-maps'); // only for common js environments
+// GoogleMapsLoader.KEY = 'AIzaSyAGe-_v3CJKidJo4RJEXAfVRrhVNnEebpU';
+// GoogleMapsLoader.load(function(google) {
+//     initMap();
+// });
 
 $("#sendTextButton").on('click', sendText);
 var requestHandler = new RequestHandler();
@@ -88,44 +94,6 @@ function displayTurbineInfo(id) {
     $("#primaryLoadCheckbox").checked = false;
     $("#windSpeedCheckbox").checked = false;
 
-
-
-    $.ajax({
-        url: restBaseUrl+"/getTurbineDataFromdayById/" + id + "/2016/1/1",
-        type: 'GET',
-        crossDomain: true,
-        dataType: 'jsonp',
-        success: function(jsonp) {
-
-            console.log(jsonp);
-            $.each(jsonp, function(rowIndex) {
-                var rowTuple = jsonp[rowIndex];
-                var tr = $("<tr/>");
-                tr.append("<td>" + rowTuple["date"] + "</td>");
-                tr.append("<td>" + rowTuple["endTime"] + "</td>");
-                tr.append("<td>" + rowTuple["windSpeed"] + "</td>");
-                tr.append("<td>" + rowTuple["ac_primary_load"] + "</td>");
-                tr.append("<td>" + rowTuple["hugh_piggott"] + "</td>");
-                tr.append("<td>" + rowTuple["ac_primary_served"] + "</td>");
-                tr.append("<td>" + rowTuple["excess_electricity"] + "</td>");
-                tr.append("<td>" + rowTuple["unmet_load"] + "</td>");
-                tr.append("<td>" + rowTuple["capacity_shortage"] + "</td>");
-                tr.append("<td>" + rowTuple["inverter_input_power"] + "</td>");
-                tr.append("<td>" + rowTuple["inverter_output_power"] + "</td>");
-                tr.append("<td>" + rowTuple["recitifier_input_power"] + "</td>");
-                tr.append("<td>" + rowTuple["rectifier_output_power"] + "</td>");
-                tr.append("<td>" + rowTuple["battery_input_power"] + "</td>");
-                tr.append("<td>" + rowTuple["battery_state_of_charge"] + "</td>");
-                tr.append("<td>" + rowTuple["battery_energy_cost"] + "</td>");
-                $("#turbineTable").append(tr);
-            });
-            $("#turbineTable").tablesorter();
-        },
-        error: function(err) {
-            console.log(err);
-        }
-    });
-
     //ADD HEATMAP
     $.ajax({
         url: restBaseUrl+"/getAverageValuesById/"+id,
@@ -190,58 +158,6 @@ function showMaintenanceLogs(start, end, id) {
     }
 }
 
-function showCubism() {
-
-    var context = cubism.context()
-    .serverDelay(0)
-    .clientDelay(0)
-    .step(1e3)
-    .size(700);
-
-    let inverter = random("Inverter Output Power");
-    let excess = random("Excess Electricity");
-    let bar = random("Wind Speed");
-    let battery = random("Battery Energy Cost");
-
-
-    d3.select("#graph").call(function(div) {
-
-      div.append("div")
-      .attr("class", "axis")
-      .call(context.axis().orient("top"));
-
-      div.selectAll(".horizon")
-      .data([inverter, bar, excess, battery]) //add inverter.subtract(bar) if you need to
-      .enter().append("div")
-      .attr("class", "horizon")
-      .call(context.horizon().extent([-20, 20]));
-
-      div.append("div")
-      .attr("class", "rule")
-      .call(context.rule());
-
-    });
-
-    function random(name) {
-      var value = 0,
-      values = [],
-      i = 0,
-      last;
-      return context.metric(function(start, stop, step, callback) {
-        start = +start, stop = +stop;
-        if (isNaN(last)) last = start;
-        while (last < stop) {
-          last += step;
-          value = Math.max(-10, Math.min(10, value + .8 * Math.random() - .4 + .2 * Math.cos(i += .2)));
-          values.push(value);
-      }
-      callback(null, values = values.slice((start - stop) / step));
-  }, name);
-  }
-  context.on("focus", function(i) {
-  d3.selectAll(".value").style("right", i == null ? null : context.size() - i + "px");
-});
-}
 
 
 function setUpTextingCheckboxes(){
@@ -298,3 +214,275 @@ function sendText(){
 }
 
 setUpTextingCheckboxes();
+
+
+
+
+
+var width = $(window).width(),
+    height = $(window).height(),
+    centered;
+
+var projection = d3.geo.mercator()
+    .scale(3600)
+    .center([33, -4.6]);
+
+var path = d3.geo.path()
+    .projection(projection)
+    .pointRadius(10);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.append("rect")
+    .attr("class", "background")
+    .attr("width", width)
+    .attr("height", height)
+    .style("stroke","black")
+    .style("stroke-width", 3)
+    .on("click", zoom);
+
+
+var g = svg.append("g");
+
+var piggott;
+var lines = [];
+$.ajax({
+        url: "http://jnorlin.me:3001/getTurbineLocations",
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function(jsonp) {
+          piggott = jsonp;
+          // buildMap();
+          initMap(null, tzaJson, weatherJson)
+          addTurbines();
+        },
+        error: function(err) {
+            console.log(err);
+            $.notify({
+                // options
+                message: 'GET Request failed!'
+            },{
+                type: 'danger'
+            }); }
+    });
+
+
+//initiates and builds the map
+function buildMap() {
+    queue()
+      .defer(d3.json, tzaJson)
+      .defer(d3.json, "../resources/data/weather.json")
+      .await(initMap);
+}
+
+function initMap(error, tza, weather) {
+  console.log(weather);
+  g.append("g")
+      .attr("id", "regions")
+    .selectAll("path")
+      .data(tza.features)
+    .enter().append("path")
+      .attr("d", path)
+      .on("click", zoom);  
+  g.selectAll("circle")
+      .data(piggott)
+      .enter()
+      .append("circle")
+      .attr("cx", function(d) {
+          return projection([d.lng, d.lat])[0];
+      })
+      .attr("cy", function(d) {
+          return projection([d.lng, d.lat])[1];
+      })
+      .attr("r", 8)
+      .attr("id", function(d) {return "circle" + d.id;})
+      .style("fill", function(d) {return d.turbineStatus ? "#60FF2D" : "red"})
+      .on("click", function(d){turbineClick(d.id)})
+      .on("mouseenter", mouseenterTurbine)
+      .on("mouseleave", mouseleaveTurbine);
+
+    // initWind(weather);
+
+    // Draw the lines
+  g.selectAll('line')
+    .data(lines)
+    .enter()
+    .append("line")
+    .attr({
+      x1: function(d) {return d.x0}, 
+      y1: function(d) {return d.y0}
+    })
+    // .style('stroke', function(d) {return colourScale(d.f);})
+    .call(lineAnimate);
+
+
+}
+function addTurbines() {
+    //make the sidepanel
+    var bar = g.selectAll("g")
+      .data(piggott)
+      .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * 51 + ")"; });
+    
+    var barWidth = 150;
+    var barHeight = 50;
+    var barx = 10;
+    var bary = 30;
+
+    //add the rectangles
+    bar.append("rect")
+      .attr("class", "turbinerect")
+      .attr("id", function(d) { return "rect"+d.id; })
+      .attr("x", barx)
+      .attr("y", bary)
+      .attr("rx", 5)
+      .attr("ry", 5)
+      .attr("width", barWidth)
+      .attr("height", barHeight - 1)
+      .on("mouseenter", mouseenterTurbine)
+      .on("mouseleave", mouseleaveTurbine);
+
+    //add text
+    bar.append("text")
+      .attr("x", 15)
+      .attr("y", (barHeight / 3) + bary)
+      .attr("dy", ".35em")
+      .style("fill", "white")
+      .text(function(d) { return "TurbineID: " + d.id; })
+      .on("mouseenter", mouseenterTurbine)
+      .on("mouseleave", mouseleaveTurbine);
+
+    //add status text
+    bar.append("text")
+      .attr("x", 15)
+      .attr("y", (2*barHeight / 3) + bary)
+      .attr("dy", ".35em")
+      .style("fill", "white")
+      .text(function(d) {return d.turbineStatus ? "Status: Working" : "Status: Not working" })
+      .on("mouseenter", mouseenterTurbine)
+      .on("mouseleave", mouseleaveTurbine);
+
+
+}
+//zooming function
+function zoom(d) {
+  console.log(d);
+  var x, y, k;
+
+  if (d && centered !== d) {
+    var centroid = path.centroid(d);
+    x = centroid[0];
+    y = centroid[1];
+    k = 4;
+    centered = d;
+  } else {
+    x = width / 2;
+    y = height / 2;
+    k = 1;
+    centered = null;
+  }
+
+  g.selectAll("path")
+      .classed("active", centered && function(d) { return d === centered; });
+
+  g.transition()
+      .duration(750)
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+      .style("stroke-width", 1.5 / k + "px");
+}
+
+
+//function for handling click on a single turbine
+function turbineClick(id) {
+  //TODO Open modal for details dashboard
+  displayTurbineInfo(id);
+
+}
+
+function mouseenterTurbine(d) {
+  d3.select("#circle" + d.id)
+    .attr("r", 12)
+    .style("stroke", "yellow");
+
+  d3.select("#rect" + d.id)
+    .style("stroke", "yellow")
+    .style("stroke-width", 5);  
+}
+
+function mouseleaveTurbine(d) {
+  d3.select("#circle" + d.id)
+    .attr("r", 8)
+    .style("stroke", "none");
+
+  d3.select("#rect" + d.id)
+    .style("stroke", "none")
+    .style("stroke-width", 0);   
+}
+
+
+function lineAnimate(selection) {
+  selection
+  .attr({
+    x2: function(d) {return d.x0},
+    y2: function(d) {return d.y0}
+  })
+  .style('opacity', 0)
+  .transition()
+    .ease('linear')
+    .duration(function(d) {return d.duration;})
+    .delay(function(d) {return d.delay;})
+    .attr({
+      x2: function(d) {return d.x1},
+      y2: function(d) {return d.y1}
+    })
+    .style('opacity', 0.8)
+  .transition()
+    .duration(1000)
+    .style('opacity', 0.1)
+  .each('end', function() {d3.select(this).call(lineAnimate)});
+}
+
+function initWind(weather) {
+  var windData = weather.list;
+  for(i = 0; i < windData.length; i++) {
+    var d = windData[i];
+    var speed = d.wind.speed;
+    var feelsLikeTemperature = d.main.temp;
+    var lonLat0 = [d.coord.lon, d.coord.lat];
+
+    // Scale line length proportionally to speed
+    var lonLat1 = lonLatFromLonLatDistanceAndBearing(lonLat0, 15 * speed, toRad(d.wind.deg));
+
+    var x0y0 = projection(lonLat0);
+    var x1y1 = projection(lonLat1);
+    var line = {
+      x0: x0y0[0],
+      y0: x0y0[1],
+      x1: x1y1[0],
+      y1: x1y1[1],
+      s: speed,
+      // f: feelsLikeTemperature,
+      duration: 8000 / speed, /* pre-compute duration */
+      delay: Math.random() * 1000 /* pre-compute delay */
+    };
+    // console.log(line);
+    lines.push(line);
+  }
+}
+
+//// MATH FUNCTIONS
+function toRad(deg) {return deg * Math.PI / 180;}
+
+function toDeg(rad) {return rad * 180 / Math.PI;}
+
+function lonLatFromLonLatDistanceAndBearing(lonLat, d, brng) {
+  // brg in radians, d in km
+  var R = 6371; // Earth's radius in km
+  var lon1 = toRad(lonLat[0]), lat1 = toRad(lonLat[1]);
+  var lat2 = Math.asin( Math.sin(lat1)*Math.cos(d/R) + Math.cos(lat1)*Math.sin(d/R)*Math.cos(brng) );
+  var lon2 = lon1 + Math.atan2(Math.sin(brng)*Math.sin(d/R)*Math.cos(lat1), Math.cos(d/R)-Math.sin(lat1)*Math.sin(lat2));
+  return [toDeg(lon2), toDeg(lat2)];
+}
